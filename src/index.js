6c00200e3289a70d25206b32b19f9dca837d3211
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-04-27 18:09:59 -0300

    Create random switch block to each round.

--- src/index.js
+++ src/index.js
@@ -1,55 +1,76 @@
 const player1 = {
-  name: "Mario",
+  name: 'Mario',
   speed: 4,
   maneuverability: 3,
   power: 3,
-  points: 0
+  points: 0,
 };
 const player2 = {
-  name: "Peach",
+  name: 'Peach',
   speed: 3,
   maneuverability: 4,
   power: 2,
-  points: 0
+  points: 0,
 };
 const player3 = {
-  name: "Yoshi",
+  name: 'Yoshi',
   speed: 2,
   maneuverability: 4,
   power: 3,
-  points: 0
+  points: 0,
 };
 const player4 = {
-  name: "Bowser",
+  name: 'Bowser',
   speed: 5,
   maneuverability: 2,
   power: 5,
-  points: 0
+  points: 0,
 };
 const player5 = {
-  name: "Luigi",
+  name: 'Luigi',
   speed: 3,
   maneuverability: 4,
   power: 4,
-  points: 0
+  points: 0,
 };
 const player6 = {
-  name: "Donkey King",
+  name: 'Donkey King',
   speed: 2,
   maneuverability: 2,
   power: 5,
-  points: 0
+  points: 0,
 };
 async function rollDice() {
-  return Math.floor((Math.random() * 6) + 1);
-};
+  return Math.floor(Math.random() * 6 + 1);
+}
+async function getRandomBlock() {
+  let random = Math.random();
+  let result;
+  switch (true) {
+    case random < 0.33:
+      result = 'RETA';
+      break;
+    case random < 0.66:
+      result = 'CURVA';
+      break;
+    default:
+      result = 'CONFRONTO';
+      break;
+  }
+  return result;
+}
 async function playRaceEngine(character1, character2) {
   for (let round = 1; round <= 5; round++) {
     console.log(`🏁 Rodada ${round}`);
-  };
-};
+    let block = await getRandomBlock();
+    console.log(`Bloco: ${block}`);
+  }
+}
 (async function main() {
-  console.log(`🏁🚨 Corrida entre ${player1.name} e ${player5.name} começando... \n`);
+  console.log(
+    `🏁🚨 Corrida entre ${player1.name} e ${player5.name} começando... \n`,
+  );
 
   await playRaceEngine(player1, player2);
+
 })();
